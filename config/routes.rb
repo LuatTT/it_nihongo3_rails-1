06b79e8c82f2326010fb00d6c9789122cd239b50
Ubuntu <ubuntu@ip-172-31-35-158.ec2.internal>
2020-11-27 07:05:04 +0000

    Add detail book

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+as :user do
+    get "signin" => "devise/sessions#new"
+    post "signin" => "devise/sessions#create"
+    delete "signout" => "devise/sessions#destroy"
+  end
